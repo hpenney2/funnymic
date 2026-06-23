65dddacddb41ac7736a521447b5968f360253313
hpenney2 <24281994+hpenney2@users.noreply.github.com>
2026-06-23 00:22:26 -0500

    fix: handle switching errors slightly more gracefully
    this still needs improvement; this will be dealt with during UI implementation
    this also adds some comments and changes the InputSwapper struct very slightly

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::{collections::HashMap, error::Error, ffi::CString};
 
 use inquire::Select;
+use pulseaudio::ClientError::ServerError;
+use pulseaudio::protocol::PulseError::NoEntity;
 use rdev::{Event, EventType, Key, listen};
-use tokio::runtime::Handle;
 
 const CONTROLLER_KEY: Key = Key::F4;
 
@@ -10,24 +11,26 @@ const CONTROLLER_KEY: Key = Key::F4;
 // though i'm not sure when we add Windows support
 pub struct InputSwapper {
     pulse_client: pulseaudio::Client,
-    original_source: String,
-    pub swap_to: String,
+    original_source: CString,
+    pub swap_to: CString,
     active: bool,
 }
 impl InputSwapper {
-    fn new(pulse_client: pulseaudio::Client, swap_to: String) -> Self {
+    pub fn new(pulse_client: pulseaudio::Client, swap_to: CString) -> Self {
         InputSwapper {
             pulse_client,
-            original_source: String::new(),
+            original_source: c"".to_owned(),
             swap_to,
             active: false,
         }
     }
 
-    async fn swap_on(&mut self) {
+    pub async fn swap_on(&mut self) {
         if self.active {
             return;
         }
+
+        println!("on...");
 
         // dear lord
         self.original_source = self
@@ -36,34 +39,44 @@ impl InputSwapper {
             .await
             .expect("failed to get defaults")
             .default_source_name
-            .unwrap()
-            .into_string()
             .unwrap();
 
-        self.pulse_client
-            .set_default_source(CString::new(self.swap_to.clone()).unwrap())
-            .await
-            .expect("failed to swap source");
+        self.switch_source(self.swap_to.clone()).await;
 
         self.active = true;
         println!("on!")
     }
 
-    async fn swap_off(&mut self) {
+    pub async fn swap_off(&mut self) {
         if !self.active {
             return;
         }
 
-        self.pulse_client
-            .set_default_source(CString::new(self.original_source.clone()).unwrap())
-            .await
-            .expect("failed to swap source");
+        println!("off...");
+
+        self.switch_source(self.original_source.clone()).await;
 
         self.active = false;
-        println!("off...")
+        println!("off!")
+    }
+
+    async fn switch_source(&self, source: CString) {
+        // TODO: not crash when failing to swap? (consider this when implementing UI?)
+        match self.pulse_client.set_default_source(source).await {
+            Ok(_) => (),
+            Err(ServerError(NoEntity)) => {
+                panic!("failed to swap source because source was removed")
+            }
+            Err(err) => panic!("failed to swap source: {:?}", err),
+        };
     }
 
     fn key_callback(&mut self, event: Event) {
+        // the only reason i do this is because rdev doesn't support asnyc functions
+        // and so we need a way to run something that *is* async (which we need because of PulseAudio's API)
+        // from sync. HOWEVER, we can do this sync using the library's lower level API.
+        // considering UI is a next step, we'll likely want async anyway and need to just rework the key listener instead
+
         match event.event_type {
             EventType::KeyPress(CONTROLLER_KEY) if !self.active => {
                 futures::executor::block_on(self.swap_on())
@@ -107,7 +120,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     )
     .prompt()?;
 
-    let mut swapper = InputSwapper::new(client, source_map.get(&funny_source).unwrap().to_owned());
+    // docs say that cloning the PulseAudio client is allowed!
+    let mut swapper = InputSwapper::new(
+        client.clone(),
+        CString::new(source_map.get(&funny_source).unwrap().to_owned())?,
+    );
 
     println!("Listening for {:?}...", CONTROLLER_KEY);
     if let Err(error) = listen(move |event| swapper.key_callback(event)) {
