6722103fa113faf07230c0df5ba2ee8a52b37c9b
hpenney2 <24281994+hpenney2@users.noreply.github.com>
2026-06-27 16:57:10 -0500

    feat(windows): set (and respect) all default device roles

--- src-tauri/src/wininputswapper.rs
+++ src-tauri/src/wininputswapper.rs
@@ -1,18 +1,24 @@
 use std::collections::HashMap;
 
 use com_policy_config::{IPolicyConfig, PolicyConfigClient};
-use windows::core::{Result as WinResult, HSTRING, PCWSTR};
+use windows::core::{Result as WinResult, HRESULT, HSTRING, PCWSTR};
 use windows::Win32::Devices::FunctionDiscovery::PKEY_Device_FriendlyName;
+use windows::Win32::Foundation::ERROR_NOT_FOUND;
 use windows::Win32::Media::Audio::{
-    eCapture, eCommunications, IMMDeviceEnumerator, MMDeviceEnumerator, DEVICE_STATE_ACTIVE,
+    eCapture, eCommunications, eConsole, eMultimedia, ERole, IMMDeviceEnumerator,
+    MMDeviceEnumerator, DEVICE_STATE_ACTIVE,
 };
 use windows::Win32::System::Com::{
     CoCreateInstance, CoInitializeEx, CoUninitialize, CLSCTX_ALL, COINIT_APARTMENTTHREADED,
     STGM_READ,
 };
 
+const E_NOTFOUND: HRESULT = HRESULT::from_win32(ERROR_NOT_FOUND.0); // this should be correct? i believe E_NOTFOUND is ERROR_NOT_FOUND wrapped in an HRESULT
+
 pub struct WinInputSwapper {
-    original_source: String,
+    original_source_console: Option<String>,
+    original_source_mm: Option<String>,
+    original_source_comms: Option<String>,
     pub swap_to: String,
 }
 impl WinInputSwapper {
@@ -23,7 +29,9 @@ impl WinInputSwapper {
                 .expect("failed to initialize COM");
         }
         WinInputSwapper {
-            original_source: String::new(),
+            original_source_console: None,
+            original_source_mm: None,
+            original_source_comms: None,
             swap_to,
         }
     }
@@ -36,10 +44,13 @@ impl WinInputSwapper {
 
         println!("on...");
 
-        // dear lord
-        self.original_source = self.get_default_device()?;
+        self.original_source_console = self.get_default_device(eConsole)?;
+        self.original_source_mm = self.get_default_device(eMultimedia)?;
+        self.original_source_comms = self.get_default_device(eCommunications)?;
 
-        self.switch_source(&self.swap_to.clone())?;
+        self.switch_source(&self.swap_to.clone(), eCommunications)?;
+        self.switch_source(&self.swap_to.clone(), eMultimedia)?;
+        self.switch_source(&self.swap_to.clone(), eConsole)?;
 
         println!("on!");
         Ok(())
@@ -48,13 +59,23 @@ impl WinInputSwapper {
     pub fn swap_off(&mut self) -> WinResult<()> {
         println!("off...");
 
-        self.switch_source(&self.original_source.clone())?;
+        if let Some(device) = &self.original_source_console {
+            self.switch_source(&device.clone(), eConsole)?;
+        }
+
+        if let Some(device) = &self.original_source_mm {
+            self.switch_source(&device.clone(), eMultimedia)?;
+        }
+
+        if let Some(device) = &self.original_source_comms {
+            self.switch_source(&device.clone(), eCommunications)?;
+        }
 
         println!("off!");
         Ok(())
     }
 
-    fn switch_source(&mut self, source: &str) -> WinResult<()> {
+    fn switch_source(&mut self, source: &str, role: ERole) -> WinResult<()> {
         // this hstring needs to be kept seperate so we don't have a dangling pointer!
         let hstring = HSTRING::from(source);
         let device_id = PCWSTR(hstring.as_ptr());
@@ -64,20 +85,21 @@ impl WinInputSwapper {
             // This is being importing in from a crate, so hopefully that will be updated if it ever changes.
             let policy_config: IPolicyConfig =
                 CoCreateInstance(&PolicyConfigClient, None, CLSCTX_ALL)?;
-            policy_config.SetDefaultEndpoint(device_id, eCommunications)?;
+            policy_config.SetDefaultEndpoint(device_id, role)?;
         };
 
         Ok(())
     }
 
-    fn get_default_device(&self) -> WinResult<String> {
+    fn get_default_device(&self, role: ERole) -> WinResult<Option<String>> {
         unsafe {
             let enumerator: IMMDeviceEnumerator =
                 CoCreateInstance(&MMDeviceEnumerator, None, CLSCTX_ALL)?;
-            Ok(enumerator
-                .GetDefaultAudioEndpoint(eCapture, eCommunications)?
-                .GetId()?
-                .to_string()?)
+            match enumerator.GetDefaultAudioEndpoint(eCapture, role) {
+                Ok(device) => Ok(Some(device.GetId()?.to_string()?)),
+                Err(err) if err.code() == E_NOTFOUND => Ok(None),
+                Err(err) => Err(err),
+            }
         }
     }
 
@@ -110,7 +132,10 @@ impl WinInputSwapper {
 
 impl Drop for WinInputSwapper {
     fn drop(&mut self) {
-        if !self.original_source.is_empty() {
+        if !self.original_source_comms.is_none()
+            || !self.original_source_mm.is_none()
+            || !self.original_source_comms.is_none()
+        {
             let _ = self.swap_off();
         }
 
